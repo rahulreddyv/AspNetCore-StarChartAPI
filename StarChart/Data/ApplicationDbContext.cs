692631a58aaa48ea6d5a6b05043ef0ec2fff7e8c
Rahul Reddy Vemireddy <ravemire@microsoft.com>
2019-12-12 15:07:41 +0530

    Added getters and setters for Celestial Object Context

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -9,6 +9,6 @@ namespace StarChart.Data
         {
         }
 
-        public DbSet<CelestialObject> celestialObjects;
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
     }
 }
